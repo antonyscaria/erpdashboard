5f0991865008474d42e6eb584572cc121bf2a6d2
Antony Scaria <antonyscaria0904@gmail.com>
2026-06-16 16:46:55 +0530

    c

--- lib/db.ts
+++ lib/db.ts
@@ -8,14 +8,13 @@ export const pool =
   globalForDb.pool ??
   mysql.createPool({
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT || 4000),
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
 
     ssl: {
       minVersion: "TLSv1.2",
-      rejectUnauthorized: true,
     },
 
     waitForConnections: true,
