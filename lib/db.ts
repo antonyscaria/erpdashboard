1837c42d9042f45e55afa09945f81e238eeeed7e
Antony Scaria <antonyscaria0904@gmail.com>
2026-06-16 17:19:01 +0530

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -7,24 +7,16 @@ const globalForDb = global as unknown as {
 export const pool =
   globalForDb.pool ??
   mysql.createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    uri: process.env.DATABASE_URL, // ❗ key change
+
+    waitForConnections: true,
+    connectionLimit: 5,
+    queueLimit: 0,
 
     ssl: {
       minVersion: "TLSv1.2",
     },
-
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
   });
-
-  console.log("DB_HOST =", process.env.DB_HOST);
-console.log("DB_USER =", process.env.DB_USER);
-console.log("DB_PORT =", process.env.DB_PORT);
 
 if (!globalForDb.pool) {
   globalForDb.pool = pool;
